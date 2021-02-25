1bfdae2f63c159b2a7898879caa6847dd158d006
Tyler Dolph <tdolph0016@myduneland.org>
2021-02-25 09:09:01 -0600

    added getID and getDaysToFund
    yeah

--- Loan.java
+++ Loan.java
@@ -1,23 +1,20 @@
-/**
- * Write a description of class loan here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Loan
 {
-    private int ID;
-    private double loanAmount;
-    private String country;
-    private int daysToFund;
-    int numLenders;
+  private int ID;
+  private double loanAmount;
+  private String country;
+  private int daysToFund;
+  int numLenders;
     
   public Loan( int ID, double loanAmount, String country, int daysToFund,
   int numLenders){
-   this.ID = ID;
-   this.loanAmount = loanAmount;
-   this.country = country;
-   this.daysToFund = daysToFund;
-   this.numLenders = numLenders;
-    }
+    this.ID = ID;
+    this.loanAmount = loanAmount;
+    this.country = country;
+    this.daysToFund = daysToFund;
+    this.numLenders = numLenders;
+  }
+
+  public int getID(){return ID;}
+  public int getDaysToFund(){return daysToFund;}
 }
