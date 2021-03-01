9e09b3b658bf2da7d35a52c16b0b76883fa25398
Tyler Dolph <tdolph0016@myduneland.org>
2021-03-01 08:53:39 -0600

    added some comments
    yeah

--- Loan.java
+++ Loan.java
@@ -1,11 +1,12 @@
 public class Loan
 {
+  //variables
   private int ID;
   private double loanAmount;
   private String country;
   private int daysToFund;
   private int numLenders;
-    
+  //instantiator 
   public Loan(int ID, double loanAmount, String country, int daysToFund,int numLenders){
     this.ID = ID;
     this.loanAmount = loanAmount;
@@ -13,12 +14,12 @@ public class Loan
     this.daysToFund = daysToFund;
     this.numLenders = numLenders;
   }
-
+  //getters
   public int getID(){return ID;}
   public double getLoanAmount(){return loanAmount;}
   public int getDaysToFund(){return daysToFund;}
   public int getNumLenders(){return numLenders;}
-
+  //setters
   public void setID(int id){ID = id;}
   public void setDaySToFund(int days){daysToFund = days;}
 }
